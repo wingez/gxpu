7c0a90007c0ad7ca34cca12f02554f43db0caeb9
gustav <gustav.ledous@gmail.com>
2021-10-26 22:39:28 +0200

    Refactor to standard gradle project structure

---
+++ src/test/kotlin/HelloWorldTest.kt
@@ -0,0 +1,13 @@
+package se.wingez
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+private class HelloWorldTest {
+    @Test
+    fun testHelloWorld(){
+        println("Hello world from tests!")
+        assertTrue(true)
+    }
+
+}
