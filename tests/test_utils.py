db043b6356e6d7a51ea3dcd6a27a101d39d052fd
gustav <gustav.ledous@gmail.com>
2021-10-27 23:47:13 +0200

    Removed the old python code

--- tests/test_utils.py
+++
@@ -1,8 +0,0 @@
-from gcpu import utils
-
-
-def test_split_many():
-    assert utils.split_many('hej', ['test']) == ['hej']
-    assert utils.split_many('hej', ['e']) == ['h', 'j']
-
-    assert utils.split_many('1,2 3,4&7', [' ', ',']) == ['1', '2', '3', '4&7']
--- tests/utils/decompile_test_error.py
+++
@@ -1,7 +0,0 @@
-import sys
-from gcpu import assembler, default_config
-
-numbers = eval(' '.join(sys.argv[1:]))
-
-for line in assembler.disassemble(default_config.instructions, numbers):
-    print(line)
