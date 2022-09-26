4f734ed3d81706c39c9072a1883b55dac6df420a
gustavld <gustav.ledous@gmail.com>
2022-09-26 22:47:56 +0200

    Added basic nodewalker

--- src/main/kotlin/astwalker/walker.kt
+++ src/main/kotlin/astwalker/walker.kt
@@ -1,2 +1,42 @@
 package se.wingez.astwalker
 
+import se.wingez.ast.AstNode
+import se.wingez.ast.NodeTypes
+
+class WalkerException() : Exception()
+
+class WalkerOutput() {
+
+    val result = mutableListOf<String>()
+}
+
+fun walk(node: AstNode, output: WalkerOutput) {
+
+    assert(node.type == NodeTypes.Function)
+
+    for (child in node.childNodes) {
+
+        when (child.type) {
+            NodeTypes.Call -> {
+                val funcNode = child.asCall()
+                assert(funcNode.targetName == "print")
+                output.result.add(getValueOf(funcNode.parameters.first()).toString())
+
+            }
+        }
+    }
+
+}
+
+fun getValueOf(node: AstNode): Int {
+
+    when (node.type) {
+        NodeTypes.Constant -> return node.asConstant()
+    }
+
+    throw WalkerException()
+}
+
+
+
+
--- src/test/kotlin/astwalker/WalkerTest.kt
+++ src/test/kotlin/astwalker/WalkerTest.kt
@@ -1,2 +1,31 @@
 package se.wingez.astwalker
+import org.junit.jupiter.api.Test
+import se.wingez.ast.function
+import se.wingez.ast.parserFromLine
+import se.wingez.compiler.buildSingleMainFunction
+import kotlin.test.assertEquals
 
+
+internal class WalkerTest{
+
+
+    @Test
+    fun testWalker(){
+
+        val function = function("main", emptyList(),
+            parserFromLine("print(5)").parseExpression(),
+            "returntype"
+        )
+
+        val output = WalkerOutput()
+
+        walk(function,output)
+        
+        assertEquals(listOf("5"),output.result)
+        
+
+
+
+    }
+
+}
