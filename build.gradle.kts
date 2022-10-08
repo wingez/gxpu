ae60e0d0442ac1f3b67b54edfd04d1e567542c20
gustavld <gustav.ledous@gmail.com>
2022-10-08 23:34:52 +0200

    Update jvm target to 16

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,13 +22,13 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "16"
 }
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "16"
 }
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "16"
 }
