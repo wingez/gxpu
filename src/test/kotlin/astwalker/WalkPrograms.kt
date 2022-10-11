2da782a7fb0c00bd2087ba8ab55bc3077f01956a
gustavld <gustav.ledous@gmail.com>
2022-10-11 12:41:20 +0200

    Walker: Program to print numbers as arrays

--- src/test/kotlin/astwalker/WalkPrograms.kt
+++ src/test/kotlin/astwalker/WalkPrograms.kt
@@ -77,8 +77,56 @@ internal class WalkPrograms {
         val nodes = parserFromFile(program).parse()
 
         assertEquals(listOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29).map { it.toString() }, walk(nodes).result)
+    }
 
+    @Test
+    fun formatInt() {
+        val program = """
+          def array_slice(arr:int[], from:int, to:int):int[]
+            
+            size = to-from
+            result=createArray(size)
+            
+            counter =0
+            while counter<size:
+              result[counter] = arr[from + counter]
+              counter = counter+1
+          
+          def format_int(value:int):int[]
+            
+            if value ==0:
+              result = "0"
+              return
+            
+            
+            max_size = 10
+            result_size = 0
+            
+            result = createArray(max_size)
+            
+            while value>0:
+              result[max_size-1-result_size] = 48+ mod(value,10)
+              value = idiv(value,10)
+              result_size = result_size +1
+            
+            
+            result = array_slice(result,max_size - result_size, max_size)
+            
+            
+          def main():
+            print(format_int(0))
+            print(format_int(1))
+            print(format_int(10))
+            print(format_int(18))
+            print(format_int(19))
+            print(format_int(207))
+            
+            
+            
+    """.trimIndent()
+        val nodes = parserFromFile(program).parse()
 
+        assertEquals(listOf(0,1,10,18,19,207).map { it.toString() }, walk(nodes).result)
     }
 
 }
