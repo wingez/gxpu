411f06f494337f73d4ae97afefeb268fef797d8a
gustav <gustav.ledous@gmail.com>
2023-08-09 19:26:08 +0200

    frontend: import builtins

--- src/main/kotlin/compiler/frontend/ProgramCompiler.kt
+++ src/main/kotlin/compiler/frontend/ProgramCompiler.kt
@@ -4,38 +4,50 @@ import compiler.BuiltInCollection
 import java.io.File
 import java.io.Reader
 
-interface FileProvider{
+interface FileProvider {
     fun getReader(filename: String): Reader
 }
 
+private const val builtins = "Builtins"
+
+
 class ProgramCompiler(
     private val fileProvider: FileProvider,
-    private val mainFile:String,
+    private val mainFile: String,
     private val builtInCollection: BuiltInCollection,
 ) {
 
+    fun compile(): CompiledFile {
 
-    fun compile():CompiledFile{
-
-        return compileFile(mainFile,fileProvider.getReader(mainFile),builtInCollection)
-
-
+        return compileFile(mainFile, fileProvider.getReader(mainFile), this)
     }
 
+    fun import(file:String):Pair<List<Datatype>, List<FunctionDefinition>>{
 
+        if (file== builtins){
+            return builtInCollection.types to builtInCollection.functions
+        }
+
+
+        TODO()
+    }
+
+    fun importBuiltins(): Pair<List<Datatype>, List<FunctionDefinition>> {
+        return import(builtins)
+    }
 
 
 }
 
 
-fun compileProgram(filename:String,builtInCollection: BuiltInCollection):CompiledFile{
+fun compileProgram(filename: String, builtInCollection: BuiltInCollection): CompiledFile {
 
-    val fileProvider = object:FileProvider{
+    val fileProvider = object : FileProvider {
         override fun getReader(f: String): Reader {
-            require(filename==f)
+            require(filename == f)
             return File(f).inputStream().reader()
         }
     }
 
-    return ProgramCompiler(fileProvider,filename,builtInCollection).compile()
+    return ProgramCompiler(fileProvider, filename, builtInCollection).compile()
 }
